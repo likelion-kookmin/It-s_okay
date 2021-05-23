b83f0eeca99a093183b7aaa034f2ffe5d33242fd
이승환 <leeseunghwan@iseunghwan-ui-MacBookPro.local>
2021-05-24 01:05:40 +0900

    add : static, base, index in templates

--- it_s_okay/config/settings.py
+++ it_s_okay/config/settings.py
@@ -56,6 +56,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "main",
+    "user.apps.UserConfig",
 ]
 
 MIDDLEWARE = [
@@ -75,7 +77,7 @@ TEMPLATE_DIR = os.path.join(BASE_DIR, "templates")
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [TEMPLATE_DIR],  # 템플릿 경로 추가
+        "DIRS": [os.path.join(BASE_DIR, 'templates')],  # 템플릿 경로 추가
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -139,3 +141,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = "/static/"
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "templates/static"),
+]
--- it_s_okay/config/urls.py
+++ it_s_okay/config/urls.py
@@ -14,8 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from main import views
+import main.urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.index),
+    path('main/', include(main.urls)),
 ]
