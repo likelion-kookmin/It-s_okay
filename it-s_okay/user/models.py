cb26d6d1549142994573bd1581022642b12d9ef2
jun-yub-kim <81306023+jun-yub-kim@users.noreply.github.com>
2021-05-24 20:47:35 +0900

    Update models.py

--- it-s_okay/user/models.py
+++ it-s_okay/user/models.py
@@ -1,16 +1,11 @@
 from django.db import models
-from django.db.models. fields import CharField, IntegerField, BooleanField, TextField, DateTimeField
+from django.contrib.auth.models import AbstractUser
 
-class User(models.Model):
-
-    email = models.CharField(max_length=40)
-    username = models.CharField(max_length=10)
-    password = models.TextField()
-    is_admin = models.BolleanField()
+class CustomUserModel(AbstractUser):
+    
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    followers = models.IntegerField()
-    followings = models.IntegerField()
+    
 
     
 # Create your models here.
