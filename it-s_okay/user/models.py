0fb680e4e7d7f4421359d7f9ad9cd0e2bac39e7e
jun-yub-kim <81306023+jun-yub-kim@users.noreply.github.com>
2021-05-24 20:26:19 +0900

    Update models.py

--- it-s_okay/user/models.py
+++ it-s_okay/user/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.db.models. fields import CharField, IntegerField, BooleanField, TextField, DateTimeField
 
+class User(models.Model):
+
+    email = models.CharField(max_length=40)
+    username = models.CharField(max_length=10)
+    password = models.TextField()
+    is_admin = models.BolleanField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    followers = models.IntegerField()
+    followings = models.IntegerField()
+
+    
 # Create your models here.
