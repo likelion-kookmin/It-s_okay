1426e5d5c9d8aefed7a78d065601e5d364949a1f
kkIIun <rlwjd4177@naver.com>
2021-05-25 01:33:26 +0900

    chore: conflict

--- it_s_okay/config/urls.py
+++ it_s_okay/config/urls.py
@@ -1,32 +1,10 @@
-"""config URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-<<<<<<< HEAD
-from django.urls import path
-
-urlpatterns = [
-    path("admin/", admin.site.urls),
-=======
 from django.urls import path, include
 from main import views
 import main.urls
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', views.index),
-    path('main/', include(main.urls)),
->>>>>>> ec3078b2b188f34b99a6210a55c8c7ba3bc40d5c
+    path("admin/", admin.site.urls),
+    path("", views.index),
+    path("main/", include(main.urls)),
 ]
--- it_s_okay/main/views.py
+++ it_s_okay/main/views.py
@@ -1,8 +1,5 @@
 from django.shortcuts import render
 
-<<<<<<< HEAD
-# Create your views here.
-=======
+
 def index(request):
     return render(request, "main/home.html")
->>>>>>> ec3078b2b188f34b99a6210a55c8c7ba3bc40d5c
