0477ddbf6710fc10081a0268c24f08819ee38d3d
kkIIun <rlwjd4177@naver.com>
2021-05-25 01:35:32 +0900

    chore: conflict

--- it_s_okay/user/models.py
+++ it_s_okay/user/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class CustomUserModel(AbstractUser):
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
