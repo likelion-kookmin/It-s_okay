26447ac7370450e459f8de76984852b3f99bb4fd
kkIIun <rlwjd4177@naver.com>
2021-05-27 02:16:06 +0900

    chore

--- it_s_okay/config/urls.py
+++ it_s_okay/config/urls.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+import main.urls
+from main import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", views.index),
+    path("main/", include(main.urls)),
 ]
