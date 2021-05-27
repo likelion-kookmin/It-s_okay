856e2d287cc4e4c796f8440384378fb6f6b64e93
이승환 <leeseunghwan@iseunghwan-ui-MacBookPro.local>
2021-05-28 02:25:51 +0900

    imporve : article

--- it_s_okay/article/urls.py
+++ it_s_okay/article/urls.py
@@ -4,5 +4,8 @@ from . import views
 app_name = 'article'
 
 urlpatterns = [
-    path('board/', views.board, name='board'),
+    # path('board/', views.board, name='board'),
+    path('list/', views.board_list, name='board_list'),
+    path('write/', views.board_write, name='board_write'),
+
 ]
--- it_s_okay/article/views.py
+++ it_s_okay/article/views.py
@@ -1,15 +1,39 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect
 from .models import Article
+from .forms import BoardForm
 
 # class PublicPostIndexView(generic.ListView): 
 #     """게시글의 목록을 표시한다.""" 
 #     model = Post
 
-def board(request):
-    return render(request, 'index/board.html')
+# def board(request):
+#     return render(request, 'index/board.html')
 
 def post(request):
     return render(request, 'post.html')
+
+def board_list(request):
+    boards = Article.objects.all().order_by('-id')
+    return render(request, 'index/board_list.html', {"boards" : boards})
+
+def board_write(request):
+    if request.method == 'POST':
+        form = BoardForm(request.POST)
+
+        if form.is_valid():
+            user_id = request.session.get('user')
+            
+            board = Article()
+            board.title = form.cleaned_data['title']
+            board.contents = form.cleaned_data['contents']
+
+            board.save()
+            return redirect('/board/list/')
+    else :
+        form = BoardForm()
+    
+    return render(request, 'index/board_write.html', {'form' : form})
+
 
 """2가지 def funcion = fbv class = cvb"""
 """일단은 함수로""" 
