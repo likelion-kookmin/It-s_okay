fc251573d90273929c3f891dda2453487a6eb1ca
jun-yub-kim <81306023+jun-yub-kim@users.noreply.github.com>
2021-06-28 13:56:10 +0900

    Update urls.py

--- it_s_okay/user/urls.py
+++ it_s_okay/user/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     path('login/', views.login, name='login'),
     path('logout/',views.logout, name='logout'),
     path('mypage/',views.mypage, name='mypage'),
+    path('change_pw/', views.change_pw, name='change_pw'),
 ]
