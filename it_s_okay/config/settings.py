515b074e7ed7a5a8abce0830d26fcaf6a83d62e6
kkIIun <rlwjd4177@naver.com>
2021-06-29 23:56:53 +0900

    fix settings

--- it_s_okay/config/settings.py
+++ it_s_okay/config/settings.py
@@ -46,7 +46,7 @@ SECRET_KEY = get_secret("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["django-env.eba-6pbiseh4.us-west-2.elasticbeanstalk.com"]
 
 
 # Application definition
